76434413f45c32485ac900852925018be70eaf55
brunofranca07 <bnfrancasant@gmail.com>
2024-07-23 12:58:47 -0300

    projeto

---
+++ codigos/login/script/script.js
@@ -0,0 +1,15 @@
+let login2=document.querySelector(".login2");
+
+
+function cadastrar(){
+    if (login2.style.display=='none')
+        login2.style.display='block'
+    else
+        login2.style.display='none'
+}
+function login(){
+    if (login2.style.display=='block')
+        login2.style.display='none'
+    else
+        login2.style.display='block'
+}
